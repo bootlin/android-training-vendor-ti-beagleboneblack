864d6ecec94d09ab7b0eca189328e738cd810617
Chris Simmonds <chris@2net.co.uk>
2014-04-16 17:13:59 +0200

    Set CPU variant as generic
    The Cortex-A8 is no longer a valid CPU variant in 4.3, so we have to use
    generic.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := generic
 
 TARGET_NO_KERNEL := true
 
